f363e27c532ee2abcf6d4471ecf604a130decbd6
Jonas Dahl <jonas@amsd.dk>
2019-02-04 10:34:18 +0100

    Added Classes

---
+++ bmiProjekt/BmiCal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bmiProjekt
+{
+    class BmiCal
+    {
+        int weight;
+        int height;
+
+        public BmiCal(int weight, int height)
+        {
+            this.weight = weight;
+            this.height = height;
+        }
+
+        public void BmiCalculator(BmiCal bmi)
+        {
+            
+        }
+    }
+}
