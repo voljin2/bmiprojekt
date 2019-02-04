90d8de217e7aaa30d39637f90b3d5cca9c15aba0
Jonas Dahl <jonas@amsd.dk>
2019-02-04 11:04:22 +0100

    Added SaveBmiCal Method

--- bmiProjekt/DB.cs
+++ bmiProjekt/DB.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace bmiProjekt
 {
@@ -10,11 +13,29 @@ namespace bmiProjekt
     {
         static public string InsertBmi(BmiCal bmi)
         {
-            string message = "";
+            string message = "no Errors";
 
+            try
+            {
 
+                XmlDocument Xml = new XmlDocument();
+                XmlSerializer Ser = new XmlSerializer(bmi.GetType());
+                MemoryStream stream = new MemoryStream();
 
-            return message;
+                Ser.Serialize(stream, bmi);
+                stream.Position = 0;
+                Xml.Load(stream);
+                Xml.Save("BMINumbers");
+                return message;
+
+            }
+            catch (Exception)
+            {
+
+                return "Error, Weight and Hight not saved";
+            }
+
+            
         }
     }
 }
